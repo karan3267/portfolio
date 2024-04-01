1be6973f55ab499571a8886d674ffb1303e96ac3
karan3267 <karanreddy3267@gmail.com>
2024-04-02 00:31:39 +0400

    testing for production error1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.port || 8000;
 const portfolioRoute = require("./routes/portfolioRoute");
 const path =require('path');
 
