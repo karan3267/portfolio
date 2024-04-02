725b2a3b7d1b5eb0db01532accf64cc17801f3f0
karan3267 <karanreddy3267@gmail.com>
2024-04-02 18:07:35 +0400

    build folder

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 3000;
 const portfolioRoute = require("./routes/portfolioRoute");
-const path =require('path');
-
+const path = require("path");
 
 app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 app.use(express.static(path.join(__dirname, "./client/build")));
+console.log(__dirname);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"), (error) => {
     res.status(500).send(error);
   });
 });
 
-app.listen(port, () => {
-  console.log("hello! server started on port:" + port);
+app.listen(port, async () => {
+  try {
+    console.log("hello! server started on port:" + port);
+  } catch (error) {
+    console.log(error);
+  }
 });
